04a6dfcd61e6846d85ac9f4f51dac28f733f3fc1
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-09 12:47:53 -0300

    import models correctly into typeorm model

--- src/presentation/app.module.ts
+++ src/presentation/app.module.ts
@@ -3,10 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { RoomingListModel } from '../infrastructure/database/models/rooming-list.model';
 import { PostgresModule } from '../infrastructure/database/postgres/postgres.module';
 import { fetchRoomingLists, roomingListsRepository } from './providers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomingListBookingModel } from 'src/infrastructure/database/models/rooming-list-bookings.model';
+import { EventModel } from 'src/infrastructure/database/models/event.model';
+import { BookingModel } from 'src/infrastructure/database/models/booking.model';
 
 @Module({
   imports: [
-    RoomingListModel,
+    TypeOrmModule.forFeature([
+      RoomingListModel,
+      EventModel,
+      BookingModel,
+      RoomingListBookingModel,
+    ]),
     PostgresModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],

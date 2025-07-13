42fb5331f74993cdfdfdd5bcbd6cdf13dc17a714
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-13 09:03:19 -0300

    make rooming lists card responsive

--- frontend/src/components/rooming-list-card.tsx
+++ frontend/src/components/rooming-list-card.tsx
@@ -14,35 +14,35 @@ export const RoomingListCard = ({ bookingsCount, cutOffDate }: Props) => {
   const day = date.getDate(); 
   
   return (
-    <div className="bg-white  border b-border  w-[400px] rounded-md p-4">
-      <div className="flex justify-between">
+    <div className="bg-white  border b-border  md:w-[400px] rounded-md p-4">
+      <div className="flex md:justify-between">
         <div className="flex flex-col">
-          <span className="font-bold text-2xl">[RFP NAME]</span>
-          <span className="text-[20px]">
+          <span className="font-bold md:text-2xl">[RFP NAME]</span>
+          <span className="md:text-[20px]">
             Agreement: <b>Staff</b>
           </span>
         </div>
         <div className="flex flex-col items-center">
           <div className="border w-fit rounded-lg ">
-            <div className="flex py-[2px] px-[10px] bg-accent-25 rounded-t-lg">
+            <div className="flex bg-accent-25 rounded-t-lg px-2  py-[2px]  md:px-[10px]">
               <span className="text-accent font-semibold text-sm">{month}</span>
             </div>
             <div className="flex justify-center bg-accent-10 rounded-b-lg">
-              <span className="text-accent font-semibold text-2xl ">{day}</span>
+              <span className="text-accent font-semibold md:text-2xl ">{day}</span>
             </div>
           </div>
-          <span className="text-muted">Cut-off Date</span>
+          <span className="text-muted text-xs">Cut-off Date</span>
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <div>
           <div className="flex gap-2">
             <img src={calendarIcon}></img>
-            <span className="text-muted tex-sm">Jan 31 - Feb, 2025</span>
+            <span className="text-muted text-[10px] md:tex-sm">Jan 31 - Feb, 2025</span>
           </div>
         </div>
         <div className="flex gap-4">
-          <Button className="text-white bg-active font-semibold w-full">
+          <Button className="text-white bg-active font-semibold md:w-full">
             View Bookings ({bookingsCount})
           </Button>
           <div className="flex justify-center  items-center border-2 border-active p-2 rounded-lg">
